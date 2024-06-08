46274f117ca01953ae39c7cb7df275f4dbd51f36
Sangrejustos <perevyshinnikita620@gmail.com>
2024-06-08 20:18:49 +0400

    some unit tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { faker } from '@faker-js/faker';
+import { UserDto } from 'src/dto/user.dto';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { UsersRepository } from 'src/user/user.repository';
+import { DatabaseService } from 'src/database/database.service';
+import { AuthModule } from './auth.module';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  let mockUserId: string;
+  let mockUser: UserDto;
+  let email: string;
+
+  beforeAll(async () => {
+    mockUserId = '1';
+    email = faker.internet.email()
+    mockUser = {
+      email,
+      login: faker.internet.userName(),
+      password: faker.internet.password(),
+      age: faker.number.int({ min: 14, max: 199 }),
+      description: faker.lorem.sentence(),
+    };
+  });
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [AuthModule],
+      providers: [AuthService, UserService, UsersRepository, DatabaseService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -15,4 +39,25 @@ describe('AuthService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe(('method registrates user'), () => {
+
+    it('should registrate user and return token', async () => {
+      expect(await service.registrate(mockUser)).toEqual({
+        token: expect.any(String),
+      });
+    })
+
+  });
+
+  describe(('method authenticates user'), () => {
+
+    it('should authenticate user and return token', async () => {
+      expect(await service.login({ email: mockUser.email, password: mockUser.password })).toEqual({
+        token: expect.any(String),
+      });
+    })
+
+  })
+
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { DatabaseService } from 'src/database/database.service';
+import { UsersRepository } from './user.repository';
+import { JwtService } from '@nestjs/jwt';
+import { UserDto } from 'src/dto/user.dto';
+import { faker } from '@faker-js/faker';
 
 describe('UserService', () => {
   let service: UserService;
 
+  let mockUserId: string;
+  let mockUser: UserDto;
+  let email: string;
+
+  beforeAll(async () => {
+    mockUserId = '1';
+    email = faker.internet.email()
+    mockUser = {
+      email,
+      login: faker.internet.userName(),
+      password: faker.internet.password(),
+      age: faker.number.int({ min: 14, max: 199 }),
+      description: faker.lorem.sentence(),
+    };
+  });
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserService, DatabaseService, UsersRepository, JwtService],
     }).compile();
+
 
     service = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('method create', () => {
+
+    it('should create user', async () => {
+
+      expect(await service.createUser(mockUser)).toEqual({ ...mockUser, id: expect.any(Number) });
+
+    })
+
   });
 });
