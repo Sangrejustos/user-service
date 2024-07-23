45ef9d24da4d96e578fbfb0c2fcc0f6585a7f3eb
Sangrejustos <perevyshinnikita620@gmail.com>
2024-07-24 00:55:24 +0400

    linter test

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
 
 @Module({
+  //some cool comment
+  //another one of cool comments
+
   imports: [ConfigModule.forRoot(), DatabaseModule, AuthModule, UserModule],
 })
 export class AppModule {}
