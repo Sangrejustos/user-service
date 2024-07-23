63a4bfab6ef7826b7085579bc1a95db55b41076b
Sangrejustos <perevyshinnikita620@gmail.com>
2024-07-24 01:07:42 +0400

    prettier config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { DatabaseModule } from "./database/database.module";
-import { AuthModule } from "./auth/auth.module";
-import { UserModule } from "./user/user.module";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  //some cool comment
-  //another one of cool comments
-
   imports: [ConfigModule.forRoot(), DatabaseModule, AuthModule, UserModule],
 })
 export class AppModule {}
